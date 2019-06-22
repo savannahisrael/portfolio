717270b2858e08c526988ac420d1aacbd8fe0cfe
Savannah Israel <sisrael0405@gmail.com>
2019-06-22 00:32:10 -0600

    html and js for all sections

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,6 @@
+
+
+
 $(window).scroll(function() {
   
   var $window = $(window),
@@ -29,6 +32,74 @@ $(window).scroll(function() {
 }).scroll();
 
 
+function codaToggleClass() {
+  $('html').toggleClass('overflow-hidden');
+  $('.main-content').toggleClass('blur');
+};
+
+/* Pop-up window script */
+function stopEmbedVideo() {
+  $('iframe').each(function() {
+       var videoSrc = $(this).attr('src');
+       $(this).attr('src','');
+       $(this).attr('src', videoSrc);
+   });
+}
+
+$('.pop-up-btn').on('click', function(e) {
+   e.preventDefault();
+   var popUpWindow = $(this).attr('href');
+   $(popUpWindow).toggleClass('pop-up-open');
+   $("nav").toggleClass("invisible");
+   if(!$(popUpWindow).hasClass('pop-up-open')){
+       stopEmbedVideo();
+   }
+   codaToggleClass();
+});
+
+$(".close-pop-up-btn").on("click", function(e) {
+  e.preventDefault();
+
+})
+
+
+if ($('.skills-bar-container').length) {
+  inView('.skills-bar-container').on('enter', function (el) {
+      $(el).find('.skill-bar').each(function () {
+          var width = $(this).find('.skill-bar-bar').width();
+          if (width < 1) {
+              $(this).find('.skill-bar-bar').animate({
+                  width: $(this).attr('data-percent') + '%',
+              }, 1500);
+              var skillPercent = $(this).attr('data-percent');
+              $(this).find('.skill-bar-percent').find('.skill-count').text(skillPercent);
+              $(this).find('.skill-count').each(function () {
+                  $(this).prop('Counter', 0).animate({
+                      Counter: $(this).text()
+                  }, {
+                      duration: 1500,
+                      easing: 'swing',
+                      step: function (now) {
+                          $(this).text(Math.ceil(now));
+                      }
+                  });
+              });
+          }
+      });
+  });
+};
+
+  // Contact form script
+  if ($('.contact-form').length) {
+    $('.contact-form .input').blur(function () {
+        if (this.value) {
+            $(this).addClass('label-up');
+        } else {
+            $(this).removeClass('label-up');
+        }
+    });
+};
+
 $(window).on('scroll', function () {
   var scroll = $(this).scrollTop();
   if ( scroll > $(window).height() ) {
@@ -37,3 +108,7 @@ $(window).on('scroll', function () {
       $('.scroll-to-top').removeClass('active-btn');
   }
 });
+
+
+
+
